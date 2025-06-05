35b3530020421b154998b9f18d122a89dfc4fb21
342348646 <342348646@C-1K2XRD2.tdsb.on.ca>
2025-06-05 11:40:52 -0400

    z

--- src/firstsketch/Mulan.java
+++ src/firstsketch/Mulan.java
@@ -10,42 +10,47 @@ import processing.core.PImage;
  *
  * @author 342348646
  */
-public class Person {
-    int x;
-    int y;
-    private String name;
-    private int age;
+public class Mulan {
+    public int x;
+    public int y;
+    private int speed;
     private PApplet app;
     private PImage image;
     private int width, height;
     
-    public Person(PApplet p, int x, int y, String name, int age, String imagePath){
+    public Mulan(PApplet p, int x, int y, int speed, String imagePath){
         this.app = p;
         this.x = x;
         this.y = y;
-        this.name = name;
-        this.age = age;
+        this.speed = speed;
         this.image = app.loadImage(imagePath);
         this.width = image.width;
         this.height = image.height;
     }
-    public boolean isCollidingWith(Person other){
+    
+    public boolean isCollidingWith(Car other){
         boolean isLeftOfOtherRight = x < other.x + other.width;
         boolean isRightOfOtherLeft = x + width > other.x;
         boolean isAboveOtherBottom = y < other.y + other.height;
         boolean isBelowOtherTop = y + height > other.y;
         return isLeftOfOtherRight && isRightOfOtherLeft && isAboveOtherBottom && isBelowOtherTop;
+    }   
+    public boolean isClicked(int mouseX, int mouseY){
+        int centerX = x+(image.pixelWidth/2);
+        int centerY = y+(image.pixelHeight/2);
+        float d = PApplet.dist(mouseX, mouseY, centerX, centerY);
+        
+        return d < 16;
+    }
+    public void displayInfo(PApplet p){
+        app.fill(0);
+        app.text("X: " + x + " Y: " + y + " Speed: " + speed, x, y-50);
     }
     public void move(int dx, int dy){
-        x += dx;
-        y += dy;
+        x+=dx;
+        y+=dy;
     }
     public void draw(){
-       app.image(image, x, y);
-    }           
-    
-    public void moveTo(int dx, int dy){
-        x = dx;
-        y = dy;
+        app.image(image, x, y);
     }
 }
